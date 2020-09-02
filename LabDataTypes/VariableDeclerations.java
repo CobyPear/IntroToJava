a968345c819a936ab482516eb98c9584d1b0238e
_Pear <63015754+CobyPear@users.noreply.github.com>
2020-09-02 09:42:02 -0500

    Java learning workspace

---
+++ LabDataTypes/VariableDeclerations.java
@@ -0,0 +1,28 @@
+package LabDataTypes;
+
+public class VariableDeclerations {
+
+    public static void main(String[] args){
+        boolean on = false;
+        System.out.println(on);
+
+        short s = 16;
+        System.out.println(s);
+
+        int i = -32;
+        System.out.println(i);
+ 
+        float f = 3839.34839f;
+        System.out.println(f);
+
+        long l = 4294967296L;
+	    System.out.println(l);
+	
+	    char c = 'a';
+        System.out.println(c);
+        
+        String w = "this is a string";
+        System.out.println(w);
+    }
+    
+}
